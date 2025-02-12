905e903469afb096ab4674752bb7a488e111daaf
Natug Mathias <princekekn@gmail.com>
2025-02-12 23:59:38 +0000

    Deleted unnecessary file(s)

--- Some.java
+++
@@ -1,8 +0,0 @@
-public class Some {
-    public static void main(String[] args){
-         int[] a = new int[10];
-         for(int i = 0; i < a.length; i++){
-            System.out.println(a[i] = ++i);
-         }
-    }
-}
